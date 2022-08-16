9343d232a3a5f635623c823645c416ceccc07838
Hamza Korkmaz <Hamzakorkmaz56@gmail.com>
2022-08-16 18:10:35 -0400

    new saved folder

---
+++ Example1.java
@@ -0,0 +1,6 @@
+public class Example1 {
+	public static void main(String[] args) {
+		 System.out.println("3.5 * 4 / 2 - 2.5 is ");
+			 System.out.println(3.5 * 4 / 2 - 2.5);
+}
+	}
--- Ham.java
+++
@@ -1,7 +0,0 @@
-// Your First Program
-
-class OLO {
-    public static void main(String[] args) {
-        System.out.println("Hello, Ham2, this is the most recent edit!"); 
-    }
-}
