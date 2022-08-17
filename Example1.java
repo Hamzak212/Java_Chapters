afc53b5bca45b297fbed76eba1e4c3552d5487a3
Hamza Korkmaz <Hamzakorkmaz56@gmail.com>
2022-08-17 00:11:28 -0400

    accident

--- Example1.java
+++ Example1.java
@@ -1,6 +1,7 @@
 public class Example1 {
 	public static void main(String[] args) {
-		 System.out.println("3.5 * 4 / 2 - 2.5 is ");
-			 System.out.println(3.5 * 4 / 2 - 2.5);
+		 System.out.println("5 + 5 = ");
+			 System.out.println(5 + 5);
+				System.out.println("amk");
 }
 	}
---
+++ gmr.java
@@ -0,0 +1,7 @@
+/* TEST */
+
+class gmr {
+    public static void main(String[] args) {
+        System.out.println("g̸m̷r̸ ̵i̵s̵ ̶l̶i̶f̵e̸!"); 
+    }
+}
