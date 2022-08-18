dfb6cdd7c4d21d38b4076298b2362f054e00829a
Hamza Korkmaz <Hamzakorkmaz56@gmail.com>
2022-08-18 14:39:28 -0400

    new

--- Example2.java
+++ Example2.java
@@ -1,6 +1,6 @@
 // Your First Program
 
-class Example2 {
+class example2 {
     public static void main(String[] args) {
         System.out.println("This is example2, if this works you are smart!"); 
     }
